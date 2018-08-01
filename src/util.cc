2bfd4e2dbee8f427f333d7989b71dca19e3ff3ff
Yura Sokolov <33312635+funny-falcon-at-joomcode@users.noreply.github.com>
2018-08-01 22:07:38 +0300

    fix isnan+isinf usage

--- src/util.cc
+++ src/util.cc
@@ -9,6 +9,7 @@
 
 #include "src/coding.h"
 #include "blackwidow/util.h"
+#include <cmath>
 
 namespace blackwidow {
 
@@ -314,9 +315,9 @@ int StrToLongDouble(const char* s, size_t slen, long double* ldval) {
 int LongDoubleToStr(long double ldval, std::string* value) {
     char buf[256];
     int len;
-    if (isnan(ldval)) {
+    if (std::isnan(ldval)) {
       return -1;
-    } else if (isinf(ldval)) {
+    } else if (std::isinf(ldval)) {
       /* Libc in odd systems (Hi Solaris!) will format infinite in a
       * different way, so better to handle it in an explicit way. */
       if (ldval > 0) {
